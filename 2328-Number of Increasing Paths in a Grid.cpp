9d172f7efa2cfbd36ce9685522d03684c3eed1ed
Feeziks <feeziksg@gmail.com>
2023-06-18 11:00:44 -0600

    Final approach, removed the second visted table and changed the early exits/checks on the dfs function.

--- 2328-Number of Increasing Paths in a Grid.cpp
+++ 2328-Number of Increasing Paths in a Grid.cpp
@@ -3,78 +3,69 @@ class Solution
 public:
     int countPaths(vector<vector<int>>& grid)
     {
-        return solve(grid) % mod;
+        return solve(grid) % 1000000007;
     }
 private:
-    long long mod = 10e9 + 7;
-
-    long long solve(vector<vector<int>> &grid)
+    int solve(vector<vector<int>> &grid)
     {
         // Init 
         int grid_width = grid[0].size();
         int grid_height = grid.size();
-        vector<vector<bool>> visited(grid_height, vector<bool>(grid_width, false));
-        vector<vector<int>> count(grid_height, vector<int>(grid_width, 0));
-        long long num_paths = 0;
+        // We can replace the "visited" vector with initing the count to -1
+        vector<vector<int>> count(grid_height, vector<int>(grid_width, -1));
+        int num_paths = 0;
 
         for(int x = 0; x < grid_width; x++)
         {
             for(int y = 0; y < grid_height; y++)
             {
-                if(visited[y][x])
+                if(count[y][x] != -1)
                 {
-                    num_paths = (num_paths + count[y][x]) % mod;
+                    num_paths = (num_paths + count[y][x]) % 1000000007;
                 }
                 else
                 {
-                    num_paths = (num_paths + depth_first_search(grid, x, y, visited, count)) % mod;
+                    num_paths = (num_paths + depth_first_search(grid, x, y, count, -1)) % 1000000007;
                 }
             }
         }
-
+        std::cout << num_paths << "\n";
         return num_paths;
     }
 
-    long long depth_first_search(vector<vector<int>> &grid, int x, int y, vector<vector<bool>> &visited, vector<vector<int>> &count)
-    {
-        //std::cout << x << "    " << y << "\n";
+    int depth_first_search(vector<vector<int>> &grid, int x, int y, vector<vector<int>> &count, int prev_value)
+    {        
+        // Check if we are trying to look outside the bounds of the grid
+        if(x < 0 || x >= grid[0].size())
+        {
+            return 0;
+        }
+        if(y < 0 || y >= grid.size())
+        {
+            return 0;
+        }
+        // Check if this path is strictly increasing
+        if(prev_value >= grid[y][x])
+        {
+            return 0;
+        }
         // Check if we have already visited this cell
-        if(visited[y][x])
+        if(count[y][x] != -1)
         {
             //std::cout << "Visited this before\n";
             return count[y][x];
         }
-        
-        long long paths = 1; // Init to 1 to include the "self" path
 
-        // Check the 4 directions
-        // North
-        if(y != 0 && grid[y - 1][x] > grid[y][x])
-        {
-            //std::cout << "North\n";
-            paths += depth_first_search(grid, x, y - 1, visited, count);
-        }
-        // East
-        if(x != grid[0].size() - 1 && grid[y][x + 1] > grid[y][x])
-        {
-            //std::cout << "East\n";
-            paths += depth_first_search(grid, x + 1, y, visited, count);
-        }
-        // South
-        if(y != grid.size() - 1 && grid[y + 1][x] > grid[y][x])
-        {
-            //std::cout << "South\n";
-            paths += depth_first_search(grid, x, y + 1, visited, count);
-        }
-        // West
-        if(x != 0 && grid[y][x - 1] > grid[y][x])
-        {
-            //std::cout << "West\n";
-            paths += depth_first_search(grid, x - 1, y, visited, count);
-        }
+        //std::cout << "x: " << x << " y: " << y << " prev_value: " << prev_value << " this_value: " << grid[y][x] << "\n";
+        
+        // Check all 4 directions
+        int north = depth_first_search(grid, x, y - 1, count, grid[y][x]);
+        int east  = depth_first_search(grid, x + 1, y, count, grid[y][x]);
+        int south = depth_first_search(grid, x, y + 1, count, grid[y][x]);
+        int west  = depth_first_search(grid, x - 1, y, count, grid[y][x]);
+
         // Our visit to this cell is done
-        visited[y][x] = true;
-        count[y][x] = paths % mod;
+        count[y][x] = (1 + north + east + south + west) % 1000000007;
         return count[y][x];
     }
 };
