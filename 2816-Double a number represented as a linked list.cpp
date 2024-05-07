c06a359e23e5d4b88ea88f2ab57de1683416e024
Feeziks <feeziksg@gmail.com>
2024-05-06 19:25:06 -0600

    2816 working solution - good enough on time and memory for me

--- 2816-Double a number represented as a linked list.cpp
+++ 2816-Double a number represented as a linked list.cpp
@@ -10,35 +10,55 @@
  */
 class Solution {
 public:
-    ListNode* doubleIt(ListNode* head) {
-        double val = 0;
-        ListNode *p1 = head;
+    ListNode* doubleIt(ListNode* head)
+    {
+        head = reverseList(head);
+
+        // Traverse backwards
+        ListNode* p1 = head;
+        int carry = 0;
         while(p1 != nullptr)
         {
-            val *= 10;
-            val += p1->val;
-            p1 = p1->next;
-        }
-
-        std::cout << "val " << val << "  val * 2 " << val * 2 << "\n";
-        val *= 2;
-
-        std::string s = std::to_string(val);
-
-        p1 = head;
-        size_t i = 0;
-        size_t len = s.length();
-        int x = 0;
-        for(i; i < len; i++)
-        {
-            p1->val = s[x++] - '0';
+          //std::cout << "val " << p1->val << " val * 2 " << p1->val*2 << "\n";
+          int val = p1->val * 2;
+          if(val >= 10)
+          {
+            //std::cout << "val > 10 adding to next node\n";
+            p1->val = val % 10 + carry;
             if(p1->next == nullptr)
             {
-              ListNode *newNode = new ListNode();
-              p1->next = newNode;
+                //std::cout << "There was no next node to add the carry value to, creating a new one\n";
+                ListNode *newNode = new ListNode();
+                p1->next = newNode;
             }
+            //std::cout << "Adding a carry value of " << val / 10 << " to the next node\n";
+            carry = val / 10;
+          }
+          else
+          {
+            p1->val = val + carry;
+            carry = 0;
+          }
+          p1 = p1->next;
         }
-        
+
+        head = reverseList(head);
         return head;
+    }
+
+    ListNode* reverseList(ListNode *head)
+    {
+      ListNode *curr = head;
+      ListNode *prev = nullptr;
+
+      while(curr != nullptr)
+      {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;        
+      }
+
+      return prev;
     }
 };
