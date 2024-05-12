87cdd67866fa6ae73f490280cdb2d66df2a984a9
Feeziks <feeziksg@gmail.com>
2024-05-12 16:11:49 -0600

    2372 reduced memory, slightly more speed. good enough

--- 2372-Largest Local Values in Matrix.cpp
+++ 2372-Largest Local Values in Matrix.cpp
@@ -3,56 +3,35 @@ class Solution
 public:
   vector<vector<int>> largestLocal(vector<vector<int>>& grid)
   {
-      std::vector<std::vector<int>> ret;
-      ret.resize(grid.size() - 2);
-      for(int x = 0; x < ret.size(); x++)
+    int gridSize = grid.size();
+    for(int i = 1; i < gridSize - 1; i++)
+    {
+      for(int j = 1; j < gridSize -1; j++)
       {
-          for(int y = 0; y < ret.size(); y++)
+        int temp = 0;
+
+        // This is basically the same exact thing as the find local max function from before but "inlined"
+        for(int x = i - 1; x <= i + 1; x++)
+        {
+          for(int y = j - 1; y <= j + 1; y++)
           {
-              ret[x].push_back(0);
+            temp = std::max(temp, grid[x][y]);
           }
+        }
+
+        grid[i - 1][j - 1] = temp;
       }
-      int max = 0;
-      int prevMax = 0;
-      bool prevMaxInWindow = false;
-      for(int x = 1; x < grid.size() - 1; x++)
-      {
-          for(int y = 1; y < grid[x].size() - 1; y++)
-          {
-              ret[x-1][y-1] = getMaxInWindow(grid, x, y);
-          }
-      }
-      //printMatrix(grid);
-      //printMatrix(ret);
-      return ret;
-  }
-  void printMatrix(vector<vector<int>> &matrix)
-  {
-      for(auto x: matrix)
-      {
-          for(auto y: x)
-          {
-              std::cout << y << " ";
-          }
-          std::cout << "\n";
-      }
-  }
-  int getMaxInWindow(std::vector<std::vector<int>> &vec, int x, int y)
-  {
-      int max = 0;
-      //std::cout << "getMaxInWindow\t";
-      //std::cout << "x: " << x << " y: " << y << "\n";
-      for(int i = x - 1; i <= x + 1; i++)
-      {
-          for(int j = y - 1; j <= y + 1; j++)
-          {
-              //std::cout << " i: " << i << " j: " << j << "\n";
-              if(vec[i][j] > max)
-              {
-                  max = vec[i][j];
-              }
-          }
-      }
-      return max;
+    }
+
+    // Saving memory by messing up the passed vector
+    // In real world use seems like a bad idea
+    // But in leet-code use where the memory is made up and we dont have co-workers we can do whatever
+    grid.resize(gridSize-2);
+    for(int i = 0; i < grid.size(); i++)
+    {
+      grid[i].resize(gridSize - 2);
+    }
+
+    return grid;
   }
 };
